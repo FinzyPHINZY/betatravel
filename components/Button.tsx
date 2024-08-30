2a2da8bfb80c40c8fd76a95af3051683c5a64afd
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-08-30 09:21:09 +0100

    feat: add GetApp section to landing page
    - Implemented GetApp component with a call-to-action for downloading the app
    - Added buttons for App Store and Play Store with appropriate icons
    - Included an image to visually represent the app on mobile devices
    - Applied responsive design for different screen sizes and layouts

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,13 +5,14 @@ interface ButtonProps {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 }
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border cursor-pointer ${variant} ${full && "w-full"}`}
     >
       {icon && <Image src={icon} alt="user icon" width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap"> {title}</label>
